9f304b17177f5443d05b35a36913de032becdc5c
Danesh M <daneshm90@gmail.com>
2013-08-06 12:32:51 -0700

    Support : Add cm support library
    Cm specific public facing api's should be placed here
    and not pollute the framework
    
    Change-Id: I1491683a7a0420e290e0fd29c4b3fd17ee59b4a0

---
+++ cm/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_MODULE := org.cyanogenmod.support
+
+include $(BUILD_STATIC_JAVA_LIBRARY)

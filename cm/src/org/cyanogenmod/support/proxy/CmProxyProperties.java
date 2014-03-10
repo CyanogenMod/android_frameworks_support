a6232ae36777d1749d8d8fb729bafe03a0136677
Danesh Mondegarian <daneshm90@gmail.com>
2014-03-09 19:35:30 -0700

    CmApi : Add GlobalProxy manager (WIP)
    Change-Id: I6ff2847a09e8099b8600cb19d585d3e3a2c0bd7c

---
+++ cm/src/org/cyanogenmod/support/proxy/CmProxyProperties.java
@@ -0,0 +1,63 @@
+package org.cyanogenmod.support.proxy;
+
+import android.net.ProxyProperties;
+
+public class CmProxyProperties {
+
+    ProxyProperties mProxyObject;
+
+    protected CmProxyProperties() {
+    }
+
+    public CmProxyProperties(String host, int port, String exclList) {
+        mProxyObject = new ProxyProperties(host, port, exclList);
+    }
+
+    void setProxyObject(ProxyProperties proxyProperties) {
+        mProxyObject = proxyProperties; 
+    }
+
+    ProxyProperties getProxyObject() {
+        return mProxyObject;
+    }
+
+    public String getHost() {
+        return mProxyObject.getHost();
+    }
+
+    public int getPort() {
+        return mProxyObject.getPort();
+    }
+
+    public String getExclusionList() {
+        return mProxyObject.getExclusionList();
+    }
+
+    public boolean isExcluded(String url) {
+        return mProxyObject.isExcluded(url);
+    }
+
+    public boolean isValid() {
+        return mProxyObject.isValid();
+    }
+
+    public java.net.Proxy makeProxy() {
+        return mProxyObject.makeProxy();
+    }
+
+    @Override
+    public String toString() {
+        return mProxyObject.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof CmProxyProperties)) return false;
+        return mProxyObject.equals(((CmProxyProperties) o).mProxyObject);
+    }
+
+    @Override
+    public int hashCode() {
+        return mProxyObject.hashCode();
+    }
+}
